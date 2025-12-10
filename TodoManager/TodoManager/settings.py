8d8d62a3f57d149ea3135438272ad82075c85edb
Mohit Pareek <mohitpareek88515@gmail.com>
2025-12-10 16:30:02 +0530

    new

--- TodoManager/TodoManager/settings.py
+++ TodoManager/TodoManager/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-local-dev-key")  # change on Render via env var
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ['todo-application-glo0.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Applications
